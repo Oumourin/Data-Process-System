4d3613ca4960e5c331131e0abe92d3d0ddb3aa38
Oumourin <32457575+Oumourin@users.noreply.github.com>
2019-05-23 22:21:42 +0800

    十进制转二进制方法

---
+++ 彭佳琪作业_数据处理系统/decimalToBinary.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// 这个算法求过大的数的二进制数可能会溢出，所以需要在初始化那里限制一下
+// 这里图方便用long long int的数组来存储结果，C11一下的编译器可以考虑换成double
+// double需要考虑输出格式的问题了
+
+void decimalToBinary(int array[11][10], long long int)
+{
+
+}
